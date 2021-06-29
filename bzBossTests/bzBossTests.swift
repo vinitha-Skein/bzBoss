32a8b701b5fe061a1472312d99f9e65372750e5a
Vinitha-Skeintech <vinitha.d@skeintech.com>
2021-06-29 11:42:02 +0530

    first commit

---
+++ bzBossTests/bzBossTests.swift
@@ -0,0 +1,33 @@
+//
+//  bzBossTests.swift
+//  bzBossTests
+//
+//  Created by Vinitha on 29/06/21.
+//
+
+import XCTest
+@testable import bzBoss
+
+class bzBossTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
